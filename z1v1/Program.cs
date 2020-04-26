dd5ddea6265264050e48c372481a3efd95c1e2cd
Anton <61160614+AHTOH2001@users.noreply.github.com>
2020-04-26 08:03:11 +0300

    Removed inheritance and other classes

--- z1v1/Program.cs
+++ z1v1/Program.cs
@@ -7,99 +7,16 @@ namespace z1v1
     {
         static void Main(string[] args)
         {
-
-            /* Human human1 = new Human(19, 61, 181, "Antoha");
-             human1.Age = 10;
-             human1.Out_info();
-             Sportsman Grisha = new Sportsman();
-
-             Grisha["Football"] = Sportsman.SetAwards(5, 2, 6);
-             Grisha["Basketball"] = Sportsman.SetAwards(-1, 1, 3);
-             // Grisha.suicide();
-             //Grisha.Award = "Garmon";
-             Grisha.Out_info();
-             Sportsman Vitek = new Sportsman
-             {
-                 Name = "Vyacheslav",
-                 Age = 24,
-                 Height = 187,
-                 Weight = 89
-             };
-             Vitek["Chess"] = Sportsman.SetAwards(15, 0, 0);
-             Vitek.Out_info();
-             Grisha.Out_info();
-             Vitek["Chess"] = Sportsman.SetAwards(3, 4, 5);
-             Vitek.Out_info();
-             try
-             {
-                 Vitek.Age = 5;
-             }
-             catch
-             {
-                 Console.WriteLine("Wrong age");
-                 return;
-             }
-             int x = 6;
-             if (x == 6)
-             {
-                 Sportsman guy = new Sportsman();
-                 guy.Age = 6;
-                 guy.Out_info();
-             }*/
-
-
-            Console.WriteLine("Enter amount of sportsmans");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Random rnd = new Random(n);
-            string[] spr = { "Basketball", "Foolball", "Voleyball", "Light athletic", "Programming", "Chess" };
-            string[] nme = { "vasya", "kolya", "vitya", "VikuSHa", "boris", "genadiy", "anton" };
-            List<Sportsman> sportsmans = new List<Sportsman>();
-            for (int i = 0; i < n; i++)
-            {
-                Human human;
-                try
-                {
-                    human = new Human(age: rnd.Next() % 100 + 1,
-                                      weight: rnd.Next() % 300 + 1,
-                                      height: rnd.Next() % 200 + 1,
-                                      name: nme[rnd.Next() % nme.Length]);
-                }
-                catch
-                {
-                    Console.WriteLine("Error");
-                    return;
-                }
-
-
-                int m = rnd.Next() % 5;
-                SpecificSport[] mas = new SpecificSport[m];
-                for (int j = 0; j < m; j++)
-                    mas[j] = new SpecificSport(new SpecificSport.Awards(rnd.Next() % 5, rnd.Next() % 5, rnd.Next() % 5), spr[rnd.Next() % spr.Length]);
-
-                sportsmans.Add(new Sportsman(human, mas));
-                //sportsmans[i][spr[rnd.Next() % spr.Length]] = new Sportsman.Award(rnd.Next() % 5, rnd.Next() % 5, rnd.Next() % 5);
-
-            }
-
-            sportsmans.Sort();
-            for (int i = 0; i < n; i++)
-                sportsmans[i].OutInfo();
-
-            Sportsman lol = new Sportsman(5, 12, 25, "Kolka");
-
-            //SpecificSport.Award y = new Sportsman.Award(3, 5, 7);// Sportsman.SetAwards(3, 5, 7);
-
-            /*Human human = new Human(14,89,130,"Liza");
-
-            Sportsman srt = new Sportsman(human,new SpecificSport(new SpecificSport.Awards(),"Gandball"));
-            //   SpecificSport srt = new SpecificSport();
-            //srt.Out_info();
-            srt.Out_info();
-
-
-            //Console.WriteLine(sportsmans[0] > sportsmans[1]);
-            */
-
+            Human human1 = new Human(29, 380, 193, "gRiSHa", true);
+            human1.OutInfo();
+            human1.Gender = false;
+            human1.Height = 173;
+            human1.Weight = 80;
+            human1.Name = "Grishila";
+            human1.DateOfBirth = new DateTime(1991, 02, 19); //but not 1991 year is forbidden;
+            human1.OutInfo();
+            Human human2 = new Human(new DateTime(2000, 05, 23), 52, 160, "Vikusha", false);
+            human2.OutInfo();
         }
     }
 }
---
+++ z1v1/human.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace z1v1
+{
+    class Human
+    {
+        private int _weight, _height;
+        private string _name;
+        private DateTime _dateOfBirth;
+        private bool _isValidDate = false;
+
+        public Human(int age, int weight, int height, string name, bool gender)
+        {
+            Age = age;
+            Weight = weight;
+            Height = height;
+            Name = name;
+            Gender = gender;
+        }
+        public Human(DateTime dateOfBirth, int weight, int height, string name, bool gender)
+        {
+            DateOfBirth = dateOfBirth;
+            Weight = weight;
+            Height = height;
+            Name = name;
+            Gender = gender;
+        }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth
+        {
+            set
+            {
+                if (DateOfBirth.Year != 1 && value.Year != DateOfBirth.Year) throw new FieldAccessException("You cannot change date of birth");
+                if (value.Year > DateTime.Now.Year || DateTime.Now.Year - value.Year >= 120) throw new IndexOutOfRangeException("Invalid age");
+                _isValidDate = true;
+                _dateOfBirth = value;
+            }
+            get => _dateOfBirth;
+        }
+        public int Age
+        {
+            private set
+            {
+                DateOfBirth = new DateTime(DateTime.Now.Year - value, 1, 1);
+                _isValidDate = false;
+            }
+            get => (int)((DateTime.Now.Ticks - DateOfBirth.Ticks) / 316224000000000);
+        }
+        public int Weight
+        {
+            set
+            {
+                if (value >= 1000 || value <= 0) throw new IndexOutOfRangeException("Invalid weight");
+                _weight = value;
+            }
+            get => _weight;
+        }
+        public int Height
+        {
+            set
+            {
+                if (value >= 300 || value <= 0) throw new IndexOutOfRangeException("Invalid Height");
+                _height = value;
+            }
+            get => _height;
+        }
+        public string Name
+        {
+            set
+            {
+                for (int i = 0; i < value.Length; i++)
+                    if (!(value[i] >= 'A' && value[i] <= 'Z') && !(value[i] >= 'a' && value[i] <= 'z'))
+                        throw new IndexOutOfRangeException("Invalid name");
+
+                if (value.Length == 0) throw new IndexOutOfRangeException("Invalid name");
+                _name = value;
+            }
+            get => FixName(_name);
+        }
+        static public string FixName(string value)
+        {
+            StringBuilder name = new StringBuilder();
+            if (value[0] >= 'A' && value[0] <= 'Z') name.Append(value[0]);
+            else name.Append((char)(value[0] - 'a' + 'A'));
+
+            for (int i = 1; i < value.Length; i++)
+                if (value[i] >= 'A' && value[i] <= 'Z') name.Append((char)(value[i] - 'A' + 'a'));
+                else name.Append(value[i]);
+            return (name.ToString());
+        }
+        public virtual void OutInfo()
+        {
+            Console.WriteLine("**************");
+            if (Gender)
+            {
+                Console.WriteLine($"Мистер {Name}{", " + (_isValidDate ? DateOfBirth.ToString("dd/MM/yyyy") + " года рождения, " : "")}вырос за последние {Age} лет,");
+                Console.WriteLine($"на {Height} сантиметров, да и растолстел на {Weight} киллограмм");
+            }
+            else
+            {
+                Console.WriteLine($"Наша любимейшая {Name}{", " + (_isValidDate ? DateOfBirth.ToString("dd/MM/yyyy") + " года рождения, " : "")}за последние {Age} лет,");
+                Console.WriteLine($"стала красивей на {Height} сантиметров, но и слегка поправилась на {Weight} киллограмм");
+            }
+        }
+
+    }
+}
